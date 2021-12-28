b290e7829a47a3f329286124b443ce188397ac99
Young37 <20171470@g.dongseo.ac.kr>
2021-12-28 17:11:17 +0900

    get/post 파라미터를 볼 수 있는 테스트 코드 추가

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/nn', function(req, res, next) {
-  console.log('test');
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).json({status: 200, query: req.query, body:req.body, message:'Success'});
+});
+
+router.post('/', function(req, res) {
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).json({status: 200, query: req.query, body:req.body, message:'Success'});
 });
 
 module.exports = router;
